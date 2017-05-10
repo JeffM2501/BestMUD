30eae0d89e7ee6ab4c0e9695b309d8db0749757f
Jeffrey Myers <JeffM2501@Gmail.com>
2017-05-10 11:20:34 -0700

    start networking listener

---
+++ Networking/ListeningManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Networking
+{
+    public static class ListeningManager
+    {
+
+        private static Dictionary<int, TcpListener> ListenPorts = new Dictionary<int, TcpListener>();
+
+        public static bool AddPort(int port)
+        {
+            if (ListenPorts.ContainsKey(port))
+                return false;
+
+            TcpListener listener = new TcpListener(IPAddress.Any, port);
+            listener.BeginAcceptTcpClient(ListenAcceptTCPClient, listener);
+            ListenPorts.Add(port, listener);
+
+            return true;
+        }
+
+        public static void StopAll()
+        {
+            foreach(var l in ListenPorts)
+            {
+                l.Value.Stop();
+            }
+        }
+
+        private static void ListenAcceptTCPClient(IAsyncResult ar)
+        {
+            try
+            {
+                TcpListener listener = ar.AsyncState as TcpListener;
+                if (listener == null || listener.Server == null)
+                    return;
+
+                var client = listener.EndAcceptTcpClient(ar);
+                listener.BeginAcceptTcpClient(ListenAcceptTCPClient, listener);
+            }
+            catch(Exception /*ex*/)
+            {
+
+            }
+        }
+    }
+}
